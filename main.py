666896a45b0b820e6e72839c8a02aa144e4a4b91
baubau <katietran.182@gmail.com>
2021-11-24 12:37:16 +0700

    steps counter and days variable

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 steps = 0
 days: List[number] = []
-
 def on_gesture_shake():
     global steps
     steps += 1
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 def on_logo_pressed():
+    global days
+    global steps
     days.append(steps)
     steps = 0
-
-input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
 let steps = 0
 let days : number[] = []
-function on_gesture_shake() {
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
     
     steps += 1
-}
-
-input.onGesture(Gesture.Shake, on_gesture_shake)
-input.onGesture(Gesture.Shake, on_gesture_shake)
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
-    let steps: number;
+    
+    
     days.push(steps)
     steps = 0
 })

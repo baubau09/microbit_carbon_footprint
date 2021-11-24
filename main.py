e1e1eb2afd99bfb89224872e949d242cd675d54c
baubau <katietran.182@gmail.com>
2021-11-24 12:56:45 +0700

    created navigation list

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 steps = 0
 days: List[number] = []
+navigation: List[string] = [
+    'Press Logo to save your record at the end of each day',
+    'Press A to view the trees saved in 30 days',
+    'Press B to view completed challenges',
+    'Press A and B together to show the record of me and my friends'
+]
+
+for option in navigation:
+    basic.show_string(option)
+
 def on_gesture_shake():
     global steps
     steps += 1
@@ -10,5 +20,8 @@ def on_logo_pressed():
     global steps
     days.append(steps)
     steps = 0
+
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
+def convert_steps():
+    global steps
--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 let steps = 0
 let days : number[] = []
+let navigation = ["Press Logo to save your record at the end of each day", "Press A to view the trees saved in 30 days", "Press B to view completed challenges", "Press A and B together to show the record of me and my friends"]
+for (let option of navigation) {
+    basic.showString(option)
+}
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
     
     steps += 1
@@ -10,3 +14,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
     days.push(steps)
     steps = 0
 })
+function convert_steps() {
+    
+}
+

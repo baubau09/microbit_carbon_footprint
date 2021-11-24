d97da90d3a12232cfa53b0ab2b3299999b0dc845
Lee Ga In <73808969+Lee-GaIn@users.noreply.github.com>
2021-11-24 16:15:46 +0700

    Add two converting functions and show_my_record function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 steps = 0
 test = 0
+tree_record = 0
+STEP_PER_KM = 1390
+CO2_PER_KM = 0.083
+CO2_PER_TREE = 21
 days: List[number] = []
 navigation: List[string] = [
     'Press Logo to save your record at the end of each day',
@@ -29,4 +33,30 @@ input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
 def convert_steps():
     global steps
+
+def convert_km_to_trees(kilometer: number):
+    total_co2 = kilometer * CO2_PER_KM
+    num_of_tree = CO2_PER_TREE / total_co2
+    return num_of_tree
+
+def convert_steps_to_trees():
+    km = steps / STEP_PER_KM
+    return convert_km_to_trees(km)
+
+def show_my_record():
+    music.start_melody(music.built_in_melody(Melodies.RINGTONE), MelodyOptions.ONCE)
+    basic.show_leds("""
+                . . # . .
+                . # # # .
+                . # # # .
+                # # # # #
+                . . # . .
+    """)
+    basic.pause(1000)
+    basic.show_number(tree_record)
+    basic.pause(2000)
+    basic.clear_screen()
+
+
+
 
--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 let steps = 0
 let test = 0
+let tree_record = 0
+let STEP_PER_KM = 1390
+let CO2_PER_KM = 0.083
+let CO2_PER_TREE = 21
 let days : number[] = []
 let navigation = ["Press Logo to save your record at the end of each day", "Press A to view the trees saved in 30 days", "Press B to view completed challenges", "Press A and B together to show the record of me and my friends"]
 basic.forever(function on_forever() {
@@ -20,5 +24,31 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
 })
 function convert_steps() {
     
+}
+
+function convert_km_to_trees(kilometer: number): number {
+    let total_co2 = kilometer * CO2_PER_KM
+    let num_of_tree = CO2_PER_TREE / total_co2
+    return num_of_tree
+}
+
+function convert_steps_to_trees(): number {
+    let km = steps / STEP_PER_KM
+    return convert_km_to_trees(km)
+}
+
+function show_my_record() {
+    music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+    basic.showLeds(`
+                . . # . .
+                . # # # .
+                . # # # .
+                # # # # #
+                . . # . .
+    `)
+    basic.pause(1000)
+    basic.showNumber(tree_record)
+    basic.pause(2000)
+    basic.clearScreen()
 }
 
